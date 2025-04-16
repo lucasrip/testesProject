ed7323d3151a348b5f91eb1c899baeae994edde4
lucasHcc <lucashccgames@gmail.com>
2025-04-16 05:02:52 -0300

    Atualizando o tipo do translate na home 2

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import { LinksContainer, LinkItem } from './styles';
 import links from '../../routes/index';
 import ComponentContainer from '../../components/ComponentContainer';
 import { useTranslation } from 'react-i18next';
+import pt from '../../routes/language/en.json';
 export default function Home() {
   const { t: translate } = useTranslation();
   return (
@@ -15,8 +16,12 @@ export default function Home() {
                 key={index + name}
                 onClick={() => window.scrollTo({ top: 0 })}
               >
-                <strong>{translate(`routes.${name}.name` as const)}</strong>
-                <p>{translate(`routes.${name}.description` as const)}</p>
+                <strong>
+                  {translate(`routes.${name}.name` as keyof typeof pt)}
+                </strong>
+                <p>
+                  {translate(`routes.${name}.description` as keyof typeof pt)}
+                </p>
               </LinkItem>
             );
           })}
