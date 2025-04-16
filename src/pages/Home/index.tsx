f1bfe1e06d71c0202da045091865ea54bdc068f1
lucasHcc <lucashccgames@gmail.com>
2025-04-16 03:13:22 -0300

    trocando versao do in18

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,9 +2,6 @@ import { LinksContainer, LinkItem } from './styles';
 import links from '../../routes/index';
 import ComponentContainer from '../../components/ComponentContainer';
 import { useTranslation } from 'react-i18next';
-import resources from './../../@types/resources';
-import { ParseKeys } from 'i18next';
-
 export default function Home() {
   const { t: translate } = useTranslation();
   return (
@@ -12,16 +9,14 @@ export default function Home() {
       <LinksContainer>
         <main>
           {links.map(({ link, name }, index) => {
-            const nameKey:ParseKeys = `routes.${name as keyof typeof resources}.name`;
-            const descriptionKey:ParseKeys = `routes.${name as keyof typeof resources}.description`;
             return (
               <LinkItem
                 to={link}
                 key={index + name}
                 onClick={() => window.scrollTo({ top: 0 })}
               >
-                <strong>{translate(nameKey)}</strong>
-                <p>{translate(descriptionKey)}</p>
+                <strong>{translate(`routes.${name}.name`)}</strong>
+                <p>{translate(`routes.${name}.description`)}</p>
               </LinkItem>
             );
           })}
