89e168b58c58d02cbba424edfcbe3f92ed97c6ba
lucasHcc <lucashccgames@gmail.com>
2025-04-16 02:39:27 -0300

    alterando o uso do translante

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,10 @@ import { LinksContainer, LinkItem } from './styles';
 import links from '../../routes/index';
 import ComponentContainer from '../../components/ComponentContainer';
 import { useTranslation } from 'react-i18next';
+import type { ParseKeys } from 'i18next';
+import  Resources  from '../../@types/resources';
+
+type RouteKeys = ParseKeys<typeof Resources, 'routes'>;
 export default function Home() {
   const { t: translate } = useTranslation();
   return (
@@ -9,14 +13,16 @@ export default function Home() {
       <LinksContainer>
         <main>
           {links.map(({ link, name }, index) => {
+              const nameKey: RouteKeys = `routes.${name}.name`;
+              const descriptionKey: RouteKeys = `routes.${name}.description`;
             return (
               <LinkItem
                 to={link}
                 key={index + name}
                 onClick={() => window.scrollTo({ top: 0 })}
               >
-                <strong>{translate(`routes.${name}.name`)}</strong>
-                <p>{translate(`routes.${name}.description`)}</p>
+                <strong>{translate(nameKey)}</strong>
+                <p>{translate(descriptionKey)}</p>
               </LinkItem>
             );
           })}
